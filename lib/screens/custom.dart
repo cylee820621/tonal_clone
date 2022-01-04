417fae6ebca00f8099dc74adffe9c5023ffa08b5
CYLee <cylee820621@gmail.com>
2022-01-03 22:14:29 -0500

    update custom page

--- lib/screens/custom.dart
+++ lib/screens/custom.dart
@@ -10,9 +10,47 @@ class Custom extends StatefulWidget {
 class _CustomState extends State<Custom> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Custom'),
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.black,
+        child: const Icon(Icons.add),
+      ),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              left: 24,
+              top: 48,
+              child: Text(
+                'Custom',
+                style: Theme.of(context).textTheme.headline3!.copyWith(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Create your own!',
+                    style: Theme.of(context).textTheme.headline5!.copyWith(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                  ),
+                  const SizedBox(height: 24),
+                  Text('Choose your moves.'),
+                  Text('Add reps, sets, and more.'),
+                  Text('Name it. Save it. Play it on reqpeat.')
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
